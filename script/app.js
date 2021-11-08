ce813a482265341d81fe9fb332f14e81a66adfc1
Benjamin Sahlgren <bensahlgren@gmail.com>
2021-11-08 15:39:18 +0100

    final commit

--- script/app.js
+++ script/app.js
@@ -48,13 +48,25 @@ const openGalleryModal = () => {
         thumbnailWrapper.innerHTML = imageSrc
           .map(
             (img) =>
-              `<img src="${img.name}" class="thumbnail" onclick="setMainImage(this.src)">`
+              `<img src="${img.name}" class="thumbnail" onclick="setStartImage(this.src)">`
           )
           .join("");
       }
       activeThumbnail();
     });
   }
+};
+
+/*
+The arrow function "setStartImage" contains a for-loop which iterates through the thumbnail-array, and sets the source
+according to the "clicked" thumbnail.
+ */
+
+const setStartImage = (src) => {
+  for (let i = 0; i < thumbnail.length; i++) {
+    startImage.setAttribute("src", src);
+  }
+  activeThumbnail();
 };
 
 /* 
@@ -70,11 +82,17 @@ const closeGalleryModal = () => {
 /*The arrow function "nextImage" contains a for-loop which iterates through each thumbnail-image. If the iterated 
 thumbnail source is equal to the startImage (the displayed image in the gallery modal) source and the iteration-value
 is not equal to thumbnail.length (total amount of thumbnails) minus 1, the source of startImage will be set to the next
-thumbnail-iteration (i += 1). */
+thumbnail-iteration (i += 1). Else if the iteration value is the last value in the array, then the source of startImage 
+will be set to the first iteration.*/
 const nextImage = () => {
   for (let i = 0; i < thumbnail.length; i++) {
     if (thumbnail[i].src == startImage.src && i !== thumbnail.length - 1) {
       startImage.setAttribute("src", thumbnail[(i += 1)].src);
+    } else if (
+      thumbnail[i].src == startImage.src &&
+      i == thumbnail.length - 1
+    ) {
+      startImage.setAttribute("src", thumbnail[0].src);
     }
   }
   activeThumbnail();
@@ -83,11 +101,16 @@ const nextImage = () => {
 /*The arrow function "prevImage" contains a for-loop which iterates through each thumbnail-image. If the iterated 
 thumbnail source is equal to the startImage (the displayed image in the gallery modal) source and the iteration-value
 is not equal to zero (the first iteration in the thumbnail-array), the source of startImage will be set to the previous
-thumbnail-iteration (i -= 1). */
+thumbnail-iteration (i -= 1). Else if the iteration value is zero then the source of startImage will be set to the last iteration. */
 const prevImage = () => {
   for (let i = 0; i < thumbnail.length; i++) {
     if (thumbnail[i].src == startImage.src && i !== 0) {
       startImage.setAttribute("src", thumbnail[(i -= 1)].src);
+    } else if (thumbnail[i].src == startImage.src && i == 0) {
+      startImage.setAttribute(
+        "src",
+        thumbnail[(i += thumbnail.length - 1)].src
+      );
     }
   }
   activeThumbnail();
@@ -100,7 +123,7 @@ non-active images.
 const activeThumbnail = () => {
   for (let i = 0; i < thumbnail.length; i++) {
     if (thumbnail[i].src === startImage.src) {
-      thumbnail[i].style.border = "1px solid white";
+      thumbnail[i].style.border = "2px solid yellow";
     } else {
       thumbnail[i].style.border = "0px";
     }
@@ -126,19 +149,19 @@ const cardContent = document.getElementById("card-modal-content");
 
 const contactPerson = [
   {
-    name: "Penguin",
+    name: "Pingu Penguin",
     role: "Zookeeper",
     number: "0701234567",
     email: "pingu.penguin@vpa.com",
   },
   {
-    name: "Dog",
+    name: "Douglas Dog",
     role: "Logistics manager",
     number: "0701234567",
     email: "douglas.dog@vpa.com",
   },
   {
-    name: "Frog",
+    name: "Freddy Frog",
     role: "Educator",
     number: "0701234567",
     email: "freddy.frog@vpa.com",
@@ -160,9 +183,9 @@ const openCardModal = () => {
       cardModal.style.display = "flex";
       for (let j = 0; j < contactPerson.length; j++) {
         if (i === j) {
-          cardContent.innerHTML = `<h2>Contact info</h2><p><strong>My name:</strong> ${contactPerson[i].name}<br><strong>My role:</strong> ${contactPerson[i].role}<br><strong>My number:</strong> ${contactPerson[i].number}
+          cardContent.innerHTML = `<h3>Contact info</h3><p><strong>My name:</strong> ${contactPerson[i].name}<br><strong>My role:</strong> ${contactPerson[i].role}<br><strong>My number:</strong> ${contactPerson[i].number}
           <br><strong>My email:</strong> ${contactPerson[i].email}</p><h3>About me</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`;
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>`;
         }
       }
     });
